ca322a32992018f4a69e9c3042c6da415c70df2a
Victor André Gris Costa <victoragcosta@hotmail.com>
2017-04-14 18:49:31 +0000

    Teste para números com algarismos até X completado, código recebeu também uma boa refatoração

--- include/romanos.h
+++ include/romanos.h
@@ -1,6 +1,14 @@
 #ifndef ROMANOS_H
 #define ROMANOS_H
 
+typedef struct tpTabela{
+	int valor;
+	char algarismo;
+}tabela;
+
 int converter(char *romano);
+int fimDaString(int posicao, char *romano);
+int casoEspecial(tabela* relacao, int tamanhoRelacao, char atual, char proximo);
+int findAlgarismo(char algarismo, tabela *relacao, int tamanho);
 
 #endif
--- src/romanos.c
+++ src/romanos.c
@@ -4,30 +4,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "romanos.h"
 
 int converter(char *romano)
 {
-	int valores[2][2];
-	valores[1][1] = 'I';
-	valores[1][2] = 1;
+	tabela valores[4];
+	valores[0].algarismo = 'I';
+	valores[0].valor = 1;
 
-	valores[2][1] = 'V';
-	valores[2][2] = 5;
+	valores[1].algarismo = 'V';
+	valores[1].valor = 5;
 
-	int i = 0, soma = 0;
-	while(romano[i] != '\0' && i <= 30){
-		if((unsigned int)i+1 < strlen(romano) && romano[i] == valores[1][1] && romano[i+1] == valores[1+1][1]){
-			soma += valores[1+1][2] - valores[1][2];
-			i+=2;
+	valores[2].algarismo = 'X';
+	valores[2].valor = 10;
+
+	valores[3].algarismo = '\0';
+	valores[3].valor = -1;
+
+	unsigned int i = 0, soma = 0;
+	while(romano[i] != '\0' && i < strlen(romano)){
+		if(!fimDaString(i+1, romano) && casoEspecial(valores, 4, romano[i], romano[i+1])){
+			soma += findAlgarismo(romano[i+1], valores, 4) - findAlgarismo(romano[i], valores, 4);
+			i++;
+		} else {
+			soma += findAlgarismo(romano[i], valores, 4);
 		}
-		if(romano[i] == valores[1][1])
-			soma += valores[1][2];
-		if(romano[i] == valores[2][1])
-			soma += valores[2][2];
 		i++;
 	}
 
 	return soma;
+}
+
+int fimDaString(int posicao, char *romano)
+{
+	return (unsigned int)posicao >= strlen(romano);
+}
+
+int casoEspecial(tabela *relacao, int tamanhoRelacao, char atual, char proximo)
+{
+	int valorAtual = findAlgarismo(atual, relacao, tamanhoRelacao);
+	int valorProx =  findAlgarismo(proximo, relacao, tamanhoRelacao);
+	return valorAtual < valorProx;
+}
+
+int findAlgarismo(char algarismo, tabela *relacao, int tamanho)
+{
+	int i = 0;
+	while(i < tamanho && relacao[i].algarismo != algarismo)
+		i++;
+	if(relacao[i].algarismo == algarismo)
+		return relacao[i].valor;
+	return -1;
 }
 
 #endif
