df0b07e8e204f06edcd89412f7708e6587416692
Victor André Gris Costa <victoragcosta@hotmail.com>
2017-04-14 15:35:17 +0000

    Teste para conversão de V completado com sucesso

--- src/romanos.c
+++ src/romanos.c
@@ -5,14 +5,19 @@
 
 int converter(char *romano)
 {
-	char valores[1][2];
+	char valores[2][2];
 	valores[1][1] = 'I';
 	valores[1][2] = 1;
+
+	valores[2][1] = 'V';
+	valores[2][2] = 5;
 
 	int i = 0, soma = 0;
 	while(romano[i] != '\0' && i <= 30){
 		if(romano[i] == valores[1][1])
 			soma += valores[1][2];
+		if(romano[i] == valores[2][1])
+			soma += valores[2][2];
 		i++;
 	}
 
